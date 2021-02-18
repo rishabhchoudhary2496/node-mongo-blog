cfaa892fe565317702c5068c1bfa96c2c6bf9d97
rishabhrishabhchoudhary2496 <rishabh_choudhary@softprodigy.com>
2021-02-18 18:15:45 +0530

    views integration-login view and home view

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const commentRoute = require('./comment')
 const replyRoute = require('./reply')
 
 module.exports = function (app) {
-  app.use('/', userRoute)
-  app.use('/blog', blogRoute)
+  app.use('/user', userRoute)
+  app.use('/', blogRoute)
   app.use('/comment', commentRoute)
   app.use('/reply', replyRoute)
 }

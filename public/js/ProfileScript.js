e0c34969dce5f2a59ec0135bd6436c860e70d878
rishabhrishabhchoudhary2496 <rishabh_choudhary@softprodigy.com>
2021-02-26 10:26:42 +0530

    renaming variablee

--- public/js/ProfileScript.js
+++ public/js/ProfileScript.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-  const readBtns = Array.from(document.querySelectorAll('#readBtn'))
+  const readButtons = Array.from(document.querySelectorAll('#readBtn'))
   let blogId
-  if (readBtns.length > 0) {
-    readBtns.forEach(function (readBtn) {
+  if (readButtons.length > 0) {
+    readButtons.forEach(function (readBtn) {
       readBtn.addEventListener('click', function () {
         blogId = this.dataset.blogid
         window.location.href = `/${blogId}`
